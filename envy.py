658429d2a806605c49504243fe34f4d213d90c02
RenatoMignella <renatoferigato@gmail.com>
2022-12-27 23:44:15 +0000

    procfile create and folders media templates static

--- envy.py
+++ envy.py
@@ -2,4 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://nxbuvlbo:nTApeAaHasswWzz1EoYg7Sn9AVURG6BU@mel.db.elephantsql.com/nxbuvlbo"
 os.environ["SECRET_KEY"] = "paoComOvoFr1t0$$$%%%%^^***DFFREGaa!!"
-# os.environ["CLOUDINARY_URL"] = "cloudinary://653553423431789:DG5tFqx9RAmgutmNxhOGPRcNlYY@dwg0i9atd"
+os.environ["CLOUDINARY_URL"] = "cloudinary://653553423431789:DG5tFqx9RAmgutmNxhOGPRcNlYY@dwg0i9atd"
