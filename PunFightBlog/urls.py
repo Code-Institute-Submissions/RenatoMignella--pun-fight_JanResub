e838e74da3ce62a6c98cf2e3170b9403d509bb8e
RenatoMignella <renatoferigato@gmail.com>
2022-12-31 00:48:36 +0000

    add authentication login and logout

--- PunFightBlog/urls.py
+++ PunFightBlog/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('', include('PunBlog.urls'), name='PunBlog-urls'), 
+    path('accounts/', include('allauth.urls')),
     # blog = PunBlog in this case
 
 ]
